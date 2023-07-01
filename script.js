17840b3c653d062c89fc4247a168061c3aa0f834
tendonitisroach <will.reynel@outlook.com>
2023-06-30 20:54:19 -0400

    first function operable

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let options = ['Rock', 'Paper', 'Scissors']
+    let randomized = console.log(options[(Math.floor(Math.random() * options.length))])
+    return randomized
+}
+getComputerChoice();
+
+function singleRound(playerSelection, computerSelection) {
+
+}
